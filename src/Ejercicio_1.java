490cda5ca30c2538f19a17cf4494701172596670
Felicitas <felicitas.aguerralde@gmail.com>
2023-05-01 18:15:22 -0300

    Modifique el Ejercicio 1

--- src/Ejercicio_1.java
+++ src/Ejercicio_1.java
@@ -1,27 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 /*Escribir un diseño de programa que mientras que el usuario ingrese un número distinto de 0,
 pida ingresar otro número y lo imprima.
  */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Ejercicio_1 {
-    public static void main (String []args){
-        int numero_1, numero_2;
-        BufferedReader entrada= new BufferedReader(new InputStreamReader(System.in));
+    public static void main (String []args) {
+        int numero_1 = obtener_numero();
+        while (numero_1!=0) {
+                int numero_2 = obtener_numero();
+                imprimir_numero(numero_2);
+                obtener_numero();
+        }
+    }
+        public static int obtener_numero() {
+        int numero=0;
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
             try {
                 System.out.println("Ingrese un numero");
-                numero_1 = Integer.valueOf(entrada.readLine());
-                while (numero_1 != 0) {
-                    System.out.println("Ingrese un numero");
-                    numero_2 = Integer.valueOf(entrada.readLine());
-                    imprimir_numero(numero_2);
-                }
+                numero = Integer.valueOf(entrada.readLine());
+            } catch (Exception exc) {
+                System.out.println(exc);
+            }
+            return numero;
+        }
 
-            }
-            catch(Exception exc){
-              System.out.println(exc);
-            }
-    }
     public static void imprimir_numero(int numero){
         System.out.println("El numero ingresado es: "+numero);
     }
